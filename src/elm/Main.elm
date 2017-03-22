eb0f8722956fd8df0aef15a3bd18de048e92111b
John Fredrik Asphaug <john.fredrik.asphaug@gmail.com>
2017-03-22 06:21:00 +0100

    Expanding with IBU calculator

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -16,6 +16,18 @@ main =
 
 -- MODEL
 type alias Model =
+  { abv : Abv
+  , error : String
+  }
+
+model : Model
+model = 
+
+    { abv = abv
+    , error = ""
+    }
+
+type alias Abv =
   { og : Float
   , fg : Float
   , miller : Float
@@ -24,11 +36,10 @@ type alias Model =
   , advanced : Float
   , alternativeAdvanced : Float
   , microbrewit : Float
-  , error : String
   }
 
-model : Model
-model = 
+abv : Abv
+abv = 
   let
     og = 1.054
     fg = 1.010
@@ -41,10 +52,8 @@ model =
     , advanced = advanced og fg
     , alternativeAdvanced = alternativeAdvanced og fg
     , microbrewit = microbrewit og fg
-    , error = ""
     }
-
--- UPDATE
+--   UPDATE
 type Msg 
   = NoOp
   | SetOG String
@@ -57,25 +66,27 @@ update msg model =
     SetOG ogString ->
       let
         og = String.toFloat ogString
-        newModel =
+        oldAbv = model.abv
+        newAbv =
           case og of
           Ok number ->
-            { model | og = number }
+            { oldAbv | og = number }
           Err error ->
-            {model | error = error } 
+            oldAbv 
       in
-        recalculate newModel
+       { model | abv = (recalculate newAbv) }
     SetFG fgString ->
       let
         fg = String.toFloat fgString
-        newModel = 
+        oldAbv = model.abv
+        newAbv = 
           case fg of
           Ok number ->
-            { model | fg = number}
+            { oldAbv | fg = number}
           Err error ->
-            {model | error = error }
+            oldAbv
       in
-        recalculate newModel
+       { model | abv = recalculate newAbv}
 
 
 -- VIEW
@@ -86,8 +97,18 @@ view model =
   div 
     [ class "content"] 
     [ div [ class "header" ] 
+          [ div [] [ text "ABV" ]
+          , div [] [ text "IBU" ]
+          ]
+    , viewAbv model.abv
+    ]
+
+viewAbv : Abv -> Html Msg
+viewAbv abv =
+  div []
+      [ div [] 
           [ h1 [] [text "ABV Calculator"] ]
-    , div [class "input-groups"] 
+      , div [class "input-groups"] 
           [ div [ class "inputs"]
                 [ label [] [ text "OG:" ]
                   , input [ type_ "number", defaultValue "1.054", onInput SetOG] [ ]
@@ -97,19 +118,20 @@ view model =
                 , input [ type_ "number", defaultValue "1.010", onInput SetFG] [ ]
                 ]
           ]
-    , div [ class "formulas"]
+      , div [ class "formulas"]
           [ div [ class "formula" ] 
                 [ div[ class "title" ] [text "Formula" ]
                 , div[ class "value" ] [text "Value"]
                 ]
-          , viewFormula "Miller:" model.miller
-          , viewFormula "Simple:" model.simple
-          , viewFormula "Alternative Simple:" model.alternativeSimple
-          , viewFormula "Advanced:" model.advanced
-          , viewFormula "Alternative Advanced:" model.alternativeAdvanced
-          , viewFormula "Microbrewit:" model.microbrewit
+          , viewFormula "Miller:" abv.miller
+          , viewFormula "Simple:" abv.simple
+          , viewFormula "Alternative Simple:" abv.alternativeSimple
+          , viewFormula "Advanced:" abv.advanced
+          , viewFormula "Alternative Advanced:" abv.alternativeAdvanced
+          , viewFormula "Microbrewit:" abv.microbrewit
           ]
-    ]
+      ]
+
 
 viewFormula : String -> Float -> Html Msg
 viewFormula formula result =
@@ -119,13 +141,13 @@ viewFormula formula result =
       ]
 
 
-recalculate : Model -> Model
-recalculate model =
-  { model 
-    | miller = miller model.og model.fg
-    , simple = simple model.og model.fg
-    , alternativeSimple = alternativeSimple model.og model.fg 
-    , advanced = advanced model.og model.fg
-    , alternativeAdvanced = alternativeAdvanced model.og model.fg
-    , microbrewit = microbrewit model.og model.fg
+recalculate : Abv -> Abv
+recalculate abv =
+  { abv 
+    | miller = miller abv.og abv.fg
+    , simple = simple abv.og abv.fg
+    , alternativeSimple = alternativeSimple abv.og abv.fg 
+    , advanced = advanced abv.og abv.fg
+    , alternativeAdvanced = alternativeAdvanced abv.og abv.fg
+    , microbrewit = microbrewit abv.og abv.fg
     }
